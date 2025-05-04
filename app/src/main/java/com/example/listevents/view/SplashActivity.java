20b55b41464d40bca7a1377deff7c413c900de2b
David <davidesparza9090@gmail.com>
2025-05-04 13:49:03 -0600

    add splashScreen and icon to app

---
+++ app/src/main/java/com/example/listevents/view/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.listevents.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.listevents.R;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, 2000); // 2 segundos
+    }
+}
